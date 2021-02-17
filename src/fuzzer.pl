393818f8da76bcf3e78df09ebd2ef843824f14db
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2021-02-17 16:03:01 -0300

    added support for param fuzzing

--- src/fuzzer.pl
+++ src/fuzzer.pl
@@ -23,7 +23,7 @@ my $recursive = 0;
 sub fuzzer_loop
 {
     my ($thr, $target, $headers, $methods, $timeout,
-        $filter, $json, $ua, $payload, $delay) = @_;
+        $filter, $json, $ua, $payload, $delay, $fuzzme) = @_;
     my $http = HTTP::Tiny->new('agent' => $ua, timeout => $timeout);
     my @req_methods = split /,/, $methods;
     my $options = {
@@ -36,10 +36,8 @@ sub fuzzer_loop
         next unless $resource;
 
         $backup_queue->enqueue($resource);
-        
-        my $full_path = $target . $resource;
-        substr($full_path, 9) =~ s/\/\/+/\//g;
-
+        my $full_path = $fuzzme ? $target =~ s/\%FUZZME\%/$resource/r :
+            substr($target . '/' . $resource, 9) =~ s/\/\/+/\//gr;
         for my $met (@req_methods)
         {
             my $response = $http->request($met, $full_path, $options);
@@ -117,6 +115,8 @@ Options:
     -f, --filter            Only display results matching with a filter
                             (See FILTERS below)
     --norecursive           Do not follow directories recursively (default)
+    --fuzzme                Enable support for parameter fuzzing
+                            (See PARAMETER FUZZING below)
 
     Examples:
         ./fuzzer.pl -w wordlist.txt -T 16 http://example.com
@@ -144,6 +144,18 @@ FILTERS:
         'status == 200; content =~ /admin/i'
         'status > 300 and status < 400 or status == 200'
 
+PARAMETER FUZZING:
+
+        When enabled, intead of appending the lines of the wordlist to the url
+    to try finding valdid endpoints on the web application, these lines will be
+    used to replace each occurency of the text \%FUZZME\% on the url.
+        This can be used to test multiple payloads for parameters of the target
+    url or to brute force certain parts of web APIs, such as user IDs.
+    
+    Examples:
+        ./fuzzer.pl -w sqli.txt --fuzzme \
+        https://target.site/page.php?id=\%FUZZME\% -f 'content =~ /SQL/'
+
 HELP
     exit 0;
 }
@@ -151,6 +163,7 @@ HELP
 sub main
 {
     my ($timeout, $tasks, $json, $delay) = (5, 10, undef, 0);
+    my $fuzzme;
     my ($useragent, $filter, $payload) = ("fuzzer.pl/0.2", "", "");
     my $methods = "GET,POST,PUT,DELETE,HEAD,TRACE,PATCH,OPTIONS,PUSH";
     my %headers;
@@ -159,6 +172,7 @@ sub main
     GetOptions(
         "h|help"        => \&help,
         "j|json"        => \$json,
+        "fuzzme"        => \$fuzzme,
         "t|tasks=i"     => \$tasks,
         "d|delay=i"     => \$delay,
         "v|version"     => \&version,
@@ -175,8 +189,15 @@ sub main
     my $target = shift @ARGV;
     die "[!] No target specified!" unless $target;
     die "[!] No wordlist specified!" unless $wordlist;
-
-    $target .= "/" if ($target =~ tr/\///) == 2;
+    
+    if ($fuzzme)
+    {
+        $recursive = 0;
+    }
+    else
+    {
+        $target .= "/" unless ($target =~ /\/$/);
+    }
 
     open my $list, "<$wordlist" || die "[!] Can't open $wordlist for reading";
     while (<$list>)
@@ -195,7 +216,7 @@ sub main
             foreach (0 .. $tasks - 1)
             {
                 threads->create('fuzzer_loop', $_, $target, \%headers, $methods,
-                    $timeout, $filter, $json, $useragent, $payload, $delay
+                    $timeout, $filter, $json, $useragent, $payload, $delay, $fuzzme
                 );
             }
         };
